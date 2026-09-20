90c9072891271dfae14d9fd6d9e7d5ae291e499d
Ethan Zhao <ethan.yzhao@outlook.com>
2026-09-20 08:44:51 +0000

    add undercooked

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     host: "127.0.0.1",
+    allowedHosts: ["localhost", "127.0.0.1", "undercooked.ethanzhao.ca"],
     port: 4173,
   },
 });
